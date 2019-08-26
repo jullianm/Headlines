460be3407214d28934794223124c8ae048a6c751
Jullian <jllnmercier@gmail.com>
2019-08-26 23:01:24 +0200

    added custom content offset method to sneak peek cells

--- Headlines/HeadlinesMain/CollectionView/CollectionViewFlowLayout.swift
+++ Headlines/HeadlinesMain/CollectionView/CollectionViewFlowLayout.swift
@@ -20,22 +20,23 @@ class CollectionViewFlowLayout: UICollectionViewFlowLayout {
         minimumLineSpacing = 10
         collectionView.backgroundColor = .clear
         scrollDirection = .horizontal
+//        sectionInset = .init(top: 0, left: 20.0, bottom: 0, right: 0)
         itemSize = .init(width: collectionView.frame.size.width/1.2, height: collectionView.frame.size.height)
         
      }
-//     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-//         // Page width used for estimating and calculating paging.
-//         let pageWidth = itemSize.width + minimumLineSpacing
-//         // Make an estimation of the current page position.
-//         let approximatePage = collectionView!.contentOffset.x/pageWidth
-//         // Determine the current page based on velocity.
-//         let currentPage = (velocity.x < 0.0) ? floor(approximatePage) : ceil(approximatePage)
-//
-//         // Calculate newHorizontalOffset.
-//         let newHorizontalOffset = ((currentPage) * pageWidth) - collectionView!.contentInset.left
-//
-//         return CGPoint(x: newHorizontalOffset, y: proposedContentOffset.y)
-//     }
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        // Page width used for estimating and calculating paging.
+        let pageWidth = itemSize.width + minimumLineSpacing
+        // Make an estimation of the current page position.
+        let approximatePage = collectionView!.contentOffset.x/pageWidth
+        // Determine the current page based on velocity.
+        let currentPage = (velocity.x < 0.0) ? floor(approximatePage) : ceil(approximatePage)
+        
+        // Calculate newHorizontalOffset.
+        let newHorizontalOffset = ((currentPage) * pageWidth) - 30
+        
+        return CGPoint(x: newHorizontalOffset, y: proposedContentOffset.y)
+    }
     
 }
 
--- Headlines/Helpers/Extensions.swift
+++ Headlines/Helpers/Extensions.swift
@@ -64,26 +64,3 @@ extension Array where Element == Category {
         return arr
     }
 }
-
-// MARK: UIImageView
-
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() {
-                self.image = image
-            }
-        }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleToFill) {  
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
-    }
-}
